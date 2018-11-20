52b5c92844eeb843aa8422abe14853f6f7bf0859
Shwingbad <38210042+Shwingbad@users.noreply.github.com>
2018-11-20 17:12:23 +0000

    Did the other 2 querires

--- views.sql
+++ views.sql
@@ -11,3 +11,22 @@ WHERE username IN(
             )
 )
 
+-- query 2 --> contains a subquery and an order by clause
+-- This query creates a leaderboard for all players based on the amount of kills they have.
+-- Should a player have 0 kills, they will not appear on the leaderboard.
+SELECT username AS Leaderboard, kills AS kills
+FROM (
+                SELECT username, kills
+                FROM stats
+                WHERE kills > 0
+) player_kills
+ORDER BY kills DESC
+
+-- query 3 --> containg a group by and having clauses
+-- This query creates a table of the weapons that are currently in any players loadout.
+SELECT primary_weapon_name
+FROM primary_weapons
+GROUP BY primary_weapon_name
+HAVING primary_weapon_name IN(
+                SELECT primary_weapon_name
+                FROM loadouts)
