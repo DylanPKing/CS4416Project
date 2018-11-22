0a6890e1154c0afe0b375801f882fc6b63389473
LouiseMadden <37147382+LouiseMadden@users.noreply.github.com>
2018-11-22 15:20:17 +0000

    procedure trigger done and mess of other trigger

--- triggers.sql
+++ triggers.sql
@@ -1,7 +1,51 @@
-DELIMiTER//
-CREATE TRIGGER makeStats
-    AFTER INSERT ON Accounts
+DELIMITER //
+CREATE FUNCTION averagekd
+    RETURNS DOUBLE
+READS SQL DATA
 BEGIN
-    INSERT INTO stats(username, kills, deaths, assists) VALUES
-    (NEW.username, 0, 0, 0);
-END;
+    DECLARE average DOUBLE
+    SET average =  (SELECT FORMAT(SUM kills)
+                    FROM stats)/
+                    (SELECT FORMAT(SUM deaths)
+                     FROM stats)
+    RETURN average
+END;//
+
+DELIMITER//
+CREATE TRIGGER avg_kd
+    AFTER UPDATE ON stats
+    FOR EACH ROW
+    CALL averagekd
+END;//
+
+DELIMITER //
+CREATE PROCEDURE statProcedure()
+BEGIN
+    INSERT INTO stats(username, kills, deaths, assists) 
+    VALUES (NEW.username, 0, 0, 0); 
+END; //
+
+DELIMITER //
+CREATE TRIGGER makeStats
+    AFTER INSERT ON Accounts 
+    FOR EACH ROW 
+BEGIN 
+    CALL statProcedure(NEW.username, 0, 0, 0);
+END; //
+
+
+
+
+DELIMITER //
+CREATE FUNCTION averagekd
+    RETURNS DOUBLE
+READS SQL DATA
+BEGIN
+    DECLARE average DOUBLE
+    DECLARE total_kills INT
+    
+    SET total_kills = SUM(USE INDEX(kills))
+                 
+   
+    RETURNS average;
+END;//
